e49146466a05635992ea752d58fcbde89b6022b6
Jonathon Robel <jonathon.robel@rfsmart.com>
2026-06-26 12:44:07 -0600

    Turn off leap autojump

--- lua/michael/plugins/leap.lua
+++ lua/michael/plugins/leap.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"https://codeberg.org/andyg/leap.nvim.git",
 		config = function()
+			require("leap").opts.safe_labels = ""
 			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 		end,
 	},
